49593d21cf00073db67035772987d8c8eb8ac42e
Gr3yMatter <jdavis2011@hotmail.co.uk>
2024-04-21 13:15:16 +0100

    multi-league scanner

--- Arbitrage Bot/arb_scanner.py
+++ Arbitrage Bot/arb_scanner.py
@@ -41,33 +41,31 @@ class OddsDataProcessor:
                         }
                         rows_list.append(row)
         return pd.DataFrame(rows_list)
-    
+
 class ValueCalculator:
     @staticmethod
     def calculate_value(df, stake, sharp_bookmakers):
         # Filter DataFrame to include only sharp bookmakers
         df_sharp = df[df['bookmaker_title'].isin(sharp_bookmakers)]
         
+        # Filter out exchange markets
+        df_non_exchange = df[~df['bookmaker_key'].isin(['betfair_ex_uk', 'betfair_ex_eu', 'matchbook', 'betfair_ex_au'])]
+        
         # Find maximum odds for each outcome from sharp bookmakers
         idx_sharp_max = df_sharp.groupby(['game_id', 'outcome_name'])['outcome_price'].idxmax()
         df_sharp_max = df_sharp.loc[idx_sharp_max].copy()
         
         # Find maximum odds for each outcome from all bookmakers
-        idx_max = df.groupby(['game_id', 'outcome_name'])['outcome_price'].idxmax()
-        df_max = df.loc[idx_max].copy()
+        idx_max = df_non_exchange.groupby(['game_id', 'outcome_name'])['outcome_price'].idxmax()
+        df_max = df_non_exchange.loc[idx_max].copy()
         
         # Merge the two DataFrames on game_id and outcome_name
         df_merged = pd.merge(df_sharp_max, df_max, on=['game_id', 'outcome_name'], suffixes=('_sharp', '_all'))
         
-        # Calculate the difference in odds between sharp and all bookmakers
-        df_merged['odds_difference'] = df_merged['outcome_price_sharp'] - df_merged['outcome_price_all']
-        
-        # Filter for positive differences indicating higher odds from sharp bookmakers
-        df_value = df_merged[df_merged['odds_difference'] > 0]
+        # Find instances where non-sharp bookmakers offer better odds
+        df_value = df_merged[df_merged['outcome_price_all'] > df_merged['outcome_price_sharp']]
         
         return df_value
-
-
 class ArbitrageCalculator:
     @staticmethod
     def calculate_arbitrage(df, stake):
@@ -76,7 +74,7 @@ class ArbitrageCalculator:
         df_arb = df.loc[idx].copy()
         df_arb['impl_prob'] = 1 / df_arb['outcome_price']
         df_arb['sum_impl_prob'] = df_arb.groupby('game_id')['impl_prob'].transform('sum')
-        # df_arb = df_arb[df_arb['sum_impl_prob'] < 0.99]
+        df_arb = df_arb[df_arb['sum_impl_prob'] < 0.99]
         df_arb['stake'] = stake / df_arb['sum_impl_prob'] * df_arb['impl_prob']
         df_arb['ror'] = (1 - df_arb['sum_impl_prob'])
         df_arb['profit'] = stake * df_arb['ror']
@@ -84,24 +82,74 @@ class ArbitrageCalculator:
 
 def main():
     api_key = 'cfbd0505443704067b3f02181ccffcde'
-    region = 'uk,eu'
+    region = 'uk'
     market = 'h2h'
-    sport_key = 'soccer_epl'
+    sport_keys = [
+                  'soccer_australia_aleague',
+                  'soccer_austria_bundesliga',
+                  'soccer_belgium_first_div',
+                  'soccer_brazil_campeonato',
+                  'soccer_brazil_serie_b',
+                  'soccer_chile_campeonato',
+                  'soccer_china_superleague',
+                  'soccer_denmark_superliga',
+                  'soccer_efl_champ',
+                  'soccer_england_league1',
+                  'soccer_england_league2',
+                  'soccer_epl',
+                  'soccer_finland_veikkausliiga',
+                  'soccer_france_ligue_one',
+                  'soccer_france_ligue_two',
+                  'soccer_germany_bundesliga',
+                  'soccer_germany_bundesliga2',
+                  'soccer_germany_liga3',
+                  'soccer_greece_super_league',
+                  'soccer_italy_serie_a',
+                  'soccer_italy_serie_b',
+                  'soccer_japan_j_league',
+                  'soccer_korea_kleague1',
+                  'soccer_league_of_ireland',
+                  'soccer_mexico_ligamx',
+                  'soccer_netherlands_eredivisie',
+                  'soccer_norway_eliteserien',
+                  'soccer_poland_ekstraklasa',
+                  'soccer_portugal_primeira_liga',
+                  'soccer_spain_la_liga',
+                  'soccer_spain_segunda_division',
+                  'soccer_spl',
+                  'soccer_sweden_allsvenskan',
+                  'soccer_sweden_superettan',
+                  'soccer_switzerland_superleague',
+                  'soccer_turkey_super_league',
+                  'soccer_uefa_europa_conference_league',
+                  'soccer_uefa_champs_league',
+                  'soccer_uefa_europa_league',
+                  'soccer_conmebol_copa_libertadores',
+                  'soccer_usa_mls',]
     stake = 1000
 
-    odds_api = OddsAPI(api_key, region, market, sport_key)
-    odds_data = odds_api.call_api()
+    merged_arbs = []
+    merged_value = []
+    
+    for sport_key in sport_keys:
+        odds_api = OddsAPI(api_key, region, market, sport_key)
+        odds_data = odds_api.call_api()
+    
+        df = OddsDataProcessor.process_data(odds_data)
+    
+        # sharp_bookmakers = ['pinnacle']
+        # df_value = ValueCalculator.calculate_value(df, stake, sharp_bookmakers)
+        # df_value.to_csv(f"Value_Against_Sharp_{sport_key}.csv")
+        # merged_value.append(df_value)
+        
+        df_arb = ArbitrageCalculator.calculate_arbitrage(df, stake)
+        merged_arbs.append(df_arb)
 
-    df = OddsDataProcessor.process_data(odds_data)
+    merged_arbs = pd.concat(merged_arbs)
+    merged_arbs.to_csv(f"All Arbitrage Opportunities.csv")
 
-    df.to_csv("Full Market View.csv")
-
-    sharp_bookmakers = ['Pinnacle']
-    df_value = ValueCalculator.calculate_value(df, stake, sharp_bookmakers)
-    df_value.to_csv("Value_Against_Sharp.csv")
-
-    df_arb = ArbitrageCalculator.calculate_arbitrage(df, stake)
-    df_arb.to_csv("Arb_Opportunities.csv")
+    # merged_value = pd.concat(merged_value)
+    # merged_value.to_csv(f"All Value Opportunities.csv")
 
 if __name__ == "__main__":
     main()
